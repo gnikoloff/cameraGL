d3b98af5affb67a4cb05200d1f5d44edadf2b587
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2017-02-13 19:11:34 +0200

    added getusermedia shim

--- app/utils/get-user-media.js
+++ app/utils/get-user-media.js
@@ -6,17 +6,14 @@ navigator.getUserMedia = navigator.getUserMedia ||
                     navigator.mozGetUserMedia;
 
 if (navigator.getUserMedia) {
-    navigator.getUserMedia({ audio: false, video: { width: 320, height: 240 } },
-    (stream) => {
+    getUserMedia({ audio: false, video: true },
+    (err, stream) => {
         let video = document.querySelector('#video');
         video.srcObject = stream;
         video.onloadedmetadata = function(e) {
             video.play();
             Events.trigger('video-access-granted')
         };
-    },
-    (err) => {
-        console.log("The following error occurred: " + err.name);
     });
 } else {
     console.log("getUserMedia not supported");
